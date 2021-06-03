f017cef92bc659c1df731f947d76a1a3e809fc51
Andres <Andres@DESKTOP-7KG6I62>
2021-06-02 19:27:34 -0600

    demo

--- src/laboratorio4/Laboratorio4.java
+++ src/laboratorio4/Laboratorio4.java
@@ -16,6 +16,7 @@ public class Laboratorio4 {
      */
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        System.out.println("continuamos trabajando");
     }
     
 }
